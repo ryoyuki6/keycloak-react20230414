595140e49fe5525e592de2dced87ad7f11943f39
R M <ryoyuki6@yahoo.co.jp>
2023-04-15 18:20:47 +0900

    resolve error

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
